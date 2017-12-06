01ade2c5a2c942759d68df60c2842e1d69cc81ba
Konailky <32402567+Konailky@users.noreply.github.com>
2017-12-06 14:37:02 -0800

    12.16.17
    I started the Lab

---
+++ Picture Lab/classes/Questions.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Questions here.
+ * I will be answering all the questions in the PictureLab here.
+ *
+ * @author (Kong)
+ * @version (12.16.17)
+ */
+public class Questions
+{
+    /**
+     * Questions:
+     * A1:
+     * 1) It takes 8 bits to represent values from 0 to 255.
+     * 2) It takes 3 bytes to represent the values of the RGB model.
+     * 3) 307200 pixels are in a picture that is 640 pixels wide and 480 pixels high. 
+     * 
+     * A2:
+     * 1) To make Pink, the RGB values are (255, 153, 255)
+     * 2) To make Yellow, the RGB values are (255, 255, 0)
+     * 3) To make Purple, the RGB values are ( 204, 0, 204)
+     * 4) To make White, the RGB values are (255, 255, 255)
+     * 5) To make Dark Gray, the RGB values are ( 51, 51, 51)
+     * 
+     * A3:
+     * 1) The row index of the top left corner is 0
+     * 2)
+     * 3)
+     * 4)
+     * 5)
+     * 6)
+     * 7)
+     * 
+     * 
+     * 
+ 
+     * 
+     * 
+     * 
+     */
+}
